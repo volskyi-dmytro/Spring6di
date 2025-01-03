f97f596b8d6bee5b317e830ba9227f44e1453e42
@stpunk47 <stpunk47@gmail.com>
2025-01-04 00:08:52 +0200

    Initial commit

---
+++ src/main/java/com/stpunk/spring6di/Spring6diApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6diApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6diApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk/spring6di/Spring6diApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6diApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
